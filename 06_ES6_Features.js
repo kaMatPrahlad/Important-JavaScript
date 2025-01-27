d3248755d8f24367f9eb3248ef4ae9de4184e23d
KaMatprahlad <kamatprahlad43@gmail.com>
2025-01-27 12:24:54 +0530

    Destructuring

--- 06_ES6_Features.js
+++ 06_ES6_Features.js
@@ -1,6 +1,6 @@
 //💥 Mind-Blowing ES6 JavaScript Techniques Every Coder Should Know
 
-// 1)  let and const Are ES6 
+// 1)  let and const Are ES6
 // 2)  Arrow Functions
 // 3)  Template Literals
 // 4)  Default Parameters
@@ -12,8 +12,20 @@
 // 10)  Async/Await
 // 11)  Try and Catch
 
- 
-
-
 //Topic of ES6
 // 1)  let and const Are ES6
+
+// 6)  Destructuring
+// Answer: Destructuring is a JavaScript expression that
+//  makes it possible to unpack values from arrays, or properties
+// from objects, into distinct variables.
+
+//Question 1:
+// var obj = { name: "prahlad", age: 21, City: "Mumbai" };
+// var { name, age, City } = obj;
+
+//Question 2:
+//Skip the second value in the array
+var arr = [1, 2, 3];
+var [a, , b] = arr;
+// console.log(a, b);
