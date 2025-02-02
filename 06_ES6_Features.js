46f2c7cdfd311f00270862b58cb7a0beff0f7873
KaMatprahlad <kamatprahlad43@gmail.com>
2025-02-02 19:38:14 +0530

    Callback Hell

--- 06_ES6_Features.js
+++ 06_ES6_Features.js
@@ -10,9 +10,10 @@
 // 8)  Modules
 // 9)  Promises
 // 10)  Async/Await
-// 11)  Try and Catch
+// 11)  Callbacks
+// 12)  Try and Catch
 
-//Topic of ES6
+//  💥  Topics of ES6   💥
 // 1)  let and const Are ES6
 
 // 6)  Destructuring
@@ -29,3 +30,98 @@
 var arr = [1, 2, 3];
 var [a, , b] = arr;
 // console.log(a, b);
+
+// 7)  Classes
+//Question 1:
+
+// 9)  Promises
+//  A promise is an object that may produce a single value some time in the future:
+// either a resolved value, or a reason that it’s not resolved (e.g., a network error occurred).
+// A promise may be in one of 3 possible states: resolved, rejected, or pending.
+
+//Question 1:
+// Answer:
+
+// var ans = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("I am resolved");
+//   }, 3000);
+// });
+
+// console.log(ans);
+
+//Question 2:
+// let promise = new Promise((resolve, reject) => {
+//   let success = true; // Simulate success or failure
+
+//   if (success) {
+//     resolve("Operation was successful!");
+//   } else {
+//     reject("Operation failed.");
+//   }
+// });
+
+// promise
+//   .then((result) => {
+//     console.log(result); // Output: Operation was successful!
+//   })
+//   .catch((error) => {
+//     console.log(error); // Output: Operation failed.
+//   });
+
+//Question 3:
+// let promise1 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("First promise resolved");
+//   }, 1000);
+// });
+
+// let promise2 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("Second promise resolved");
+//   }, 2000);
+// });
+
+// Promise.all([promise1, promise2])
+//   .then((results) => {
+//     console.log(results); // Output: ["First promise resolved", "Second promise resolved"]
+//   })
+//   .catch((error) => {
+//     console.log("Error:", error);
+//   });
+
+// 11)  Callbacks
+
+//Callback Hell:
+// What is Callback Hell?
+// Callback Hell is an anti-pattern in programming where a sequence of nested callbacks
+// makes the code hard to read and debug. It is common in asynchronous programming with
+// callbacks. Callback Hell is also known as the Pyramid of Doom due to its shape.
+
+
+
+//Question 1:
+function getData(dataId, getNextData) {
+  setTimeout(() => {
+    console.log("Data is ", dataId);
+    if (getNextData) {
+      getNextData();
+    }
+  }, 2000);
+}
+
+getData(2, () => {
+  getData(3, () => {
+    getData(4);
+  });
+});
+
+// 12) Try and catch
+//Question 1:
+// try {
+//   console.log("Start of try runs");
+//   unicycle;
+//   console.log("End of try runs -- never reached");
+// } catch (error) {
+//   console.log("Caught an error:", error);
+// }
