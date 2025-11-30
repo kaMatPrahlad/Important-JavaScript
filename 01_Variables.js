39d0c73c400ff3b28581021b9c78b253f7a6e000
kaMatPrahlad <kamatprahlad43@gmail.com>
2025-12-01 01:04:59 +0530

    updated

--- 01_Variables.js
+++ 01_Variables.js
@@ -1,31 +1,26 @@
-// 1) Variables in JS 
+// 1) Variables in JS
 
 var a = 5;
 var b = 2;
 // console.log(a + b)
 // console.log(typeof a)
 
+// Question no 1
 
-
-// Question no 1 
-
-// const product = {
-//     name: "ball pen",
-//     color: "blue",
-//     price: 10,
-//     isAvailable: true,
-
-
-// }
-// console.log(product["name"])
-
+const product = {
+  name: "ball pen",
+  color: "blue",
+  price: 10,
+  isAvailable: true,
+};
+console.log(product["name"]);
 
 // Question no 2
-const profile = {
-    userName: "Prahlad",
-    age: 21,
-    followers: 1000,
-    isFollowing: true,
-}
+// const profile = {
+//     userName: "Prahlad",
+//     age: 21,
+//     followers: 1000,
+//     isFollowing: true,
+// }
 
-console.log(typeof profile)
+// console.log(typeof profile)
