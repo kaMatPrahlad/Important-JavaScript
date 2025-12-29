9021151aa3eff3a1187ff19cd3b1a5b904f4d2f6
kaMatPrahlad <kamatprahlad43@gmail.com>
2025-12-29 13:15:29 +0530

    Update DOM examples and content in HTML and JavaScript files

--- 06_DOM.js
+++ 06_DOM.js
@@ -5,3 +5,18 @@
 // 👉 The DOM is an object-oriented representation of the web page, which can be modified with a scripting language such as JavaScript.
 
 // Example 1)
+
+const h2 = document.querySelector("h2");
+console.log(h2.innerText); // Hello Javascript!
+
+h2.innerText = h2.innerText + "Hello DOM!";
+// console.log(h2.innerText); // Hello DOM!
+
+// Example 2)
+
+const divs = document.querySelectorAll("div");
+// Accessing individual div elements
+// console.log(divs[2]); // NodeList(3) [div, div, div]
+// <div>Modified Second div</div>
+divs[1].innerText = "Modified Second div";
+// console.log(divs[1]);
