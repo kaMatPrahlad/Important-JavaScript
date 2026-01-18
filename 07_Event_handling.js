d2a18561b245886c179ea9a5702492a6d59e86ed
kaMatPrahlad <kamatprahlad43@gmail.com>
2026-01-18 21:43:29 +0530

    Event handling

--- 07_Event_handling.js
+++ 07_Event_handling.js
@@ -7,10 +7,30 @@ btn.onclick = () => {
   console.log(a);
 };
 
-let div = document.querySelector("div");
-div.onmouseover = (e) => {
-  console.log(e);
-  console.log(e.type);
-  console.log(e.target);
-  console.log(e.clientX, e.clientY);
-};
+// let div = document.querySelector("div");
+// div.onmouseover = (e) => {
+//   console.log(e);
+//   //   console.log(e.type);
+//   //   console.log(e.target);
+//   //   console.log(e.clientX, e.clientY);
+// };
+
+// Let‘s Practice
+
+// Qs. Create a toggle button that changes the screen to dark-mode when clicked & light-mode
+// when clicked again.
+// Ans=>
+
+let toggleBtn = document.querySelector("#mode");
+let currentMode = "light";
+
+toggleBtn.addEventListener("click", () => {
+  if (currentMode === "light") {
+    currentMode = "dark";
+    document.body.style.backgroundColor = "black";
+  } else {
+    currentMode = "light";
+    document.body.style.backgroundColor = "white";
+  }
+  console.log("Current Mode:", currentMode);
+});
