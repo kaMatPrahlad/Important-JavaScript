15a828ccb26fb295c60b785d75ded54ea5d26d0d
kaMatPrahlad <kamatprahlad43@gmail.com>
2026-01-21 19:58:09 +0530

    updated logic

--- 08_index.js
+++ 08_index.js
@@ -35,7 +35,20 @@ boxes.forEach((box) => {
 });
 
 const checkWin = () => {
+  // Whole logic to check winning patterns
+
   for (let pattern of winPatterns) {
-    console.log(pattern[0], pattern[1], pattern[2]);
+    // console.log(pattern[0], pattern[1], pattern[2]);
+    // Get the values of the 3 boxes
+    // Check if they are same, not empty, then we have a winner
+    let post1Val = boxes[pattern[0]].innerText;
+    let post2Val = boxes[pattern[1]].innerText;
+    let post3Val = boxes[pattern[2]].innerText;
+    /// Check if they are same and not empty
+    if (post1Val != "" && post2Val != "" && post3Val != "") {
+      if (post1Val == post2Val && post2Val == post3Val) {
+        console.log("We have a winner: " + post1Val);
+      }
+    }
   }
 };
