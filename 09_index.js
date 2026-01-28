e93a904d47819709dcd2e3603c261fd2f5b1aa4d
kaMatPrahlad <kamatprahlad43@gmail.com>
2026-01-28 20:31:26 +0530

    Git updated

---
+++ 09_index.js
@@ -0,0 +1,11 @@
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+
+choices.forEach(("click")=>{
+    choices.ad((choice))
+})
+
+let userScore = document.getElementById("#user-score");
+let compScore = document.getElementById("#comp-score");
