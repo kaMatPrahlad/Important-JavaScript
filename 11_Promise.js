1bd5e3010aabdfc1ea101cfa79b058c96b2a42b8
kaMatPrahlad <kamatprahlad43@gmail.com>
2026-02-01 14:01:35 +0530

    added code

--- 11_Promise.js
+++ 11_Promise.js
@@ -7,13 +7,30 @@
 // 3. Rejected: The operation failed.
 
 // Creating a Promise
-let promise = new Promise((Reslove, Reject) => {
-  console.log("Promise is pending");
-  //   Reslove("Promise is Resloved");
-  Reject("Some error occurred");
-});
-// console.log(promise);
+// let promise = new Promise((Reslove, Reject) => {
+//   console.log("Promise is pending");
+//   //   Reslove("Promise is Resloved");
+//   Reject("Some error occurred");
+// });
+// // console.log(promise);
 
-setTimeout(() => {
-  console.log(promise);
-}, 3000);
+// setTimeout(() => {
+//   console.log(promise);
+// }, 3000);
+
+const getPromise = () => {
+  return new Promise((reslove, reject) => {
+    console.log("I am a Promise");
+    reslove("Promise is Resloved");
+    // reject("Some error occurred");
+  });
+};
+
+let promise = getPromise();
+// result and error are parameters
+promise.then((result) => {
+  console.log("Promise Fulfilled", result);
+});
+promise.catch((error) => {
+  console.log("Promise Rejected", error);
+});
