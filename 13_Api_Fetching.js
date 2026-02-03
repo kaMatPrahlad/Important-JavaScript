917ae4258e43aaa396e4fb48e696193b2c92ff10
kaMatPrahlad <kamatprahlad43@gmail.com>
2026-02-03 14:14:12 +0530

    Refactor API fetching example and enhance Currency Converter functionality with dropdown options and exchange rate updates

--- 13_Api_Fetching.js
+++ 13_Api_Fetching.js
@@ -5,13 +5,43 @@
 
 // Example of API Fetching using the Fetch API
 
-const URL = "https://dog.ceo/api/breeds/image/random";
+// const URL = "https://dog.ceo/api/breeds/image/random";
 
-const getFacts = async () => {
-  console.log("Fetching Data.....");
-  let response = await fetch(URL);
-  console.log(response);
-  let data = await response.json();
+// const getFacts = async () => {
+//   console.log("Fetching Data.....");
+//   let response = await fetch(URL);
+//   console.log(response);
+//   let data = await response.json();
+//   console.log(data);
+// };
+
+// async function main() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("Data is Resolved");
+//     }, 3000);
+//   });
+// }
+
+// let data = main();
+// data.then((ans) => {
+//   console.log("Show Data", ans);
+// });
+// data.catch((uttar) => {
+//   console.log("Data Rejected", uttar);
+// });
+
+async function getData() {
+  let x = await fetch("https://dog.ceo/api/breeds/image/random");
+  let data = await x.json();
   console.log(data);
-};
+}
 
+async function main() {
+  console.log("Loading Data");
+  let p1 = await getData();
+  console.log(p1);
+  console.log("Final Data");
+}
+
+main();
---
+++ 15_Practice.js
@@ -0,0 +1,80 @@
+//1- Print 0 to 5 Using a Loop
+// i = 0;
+// for (i < 0; i <= 5; i++) {
+//   console.log(i);
+// }
+
+// i = 1;
+// while (i <= 5) {
+//   console.log(i);
+//   i++;
+// }
+
+// ?? Using Do While Loop
+// i = 1;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 5);
+
+//2- Write a Function to add two numbers and return the result
+// num1 = 3;
+// num2 = 4;
+// function add(num1, num2) {
+//   return num1 + num2;
+// }
+// let ans = add(7, 4);
+// console.log(ans);
+
+//3-
+
+// let calculateAreaofRectangle = (width, height) => {
+//   return width * height;
+// };
+// // let sum = calculateAreaofRectangle(5, 70);
+// // console.log("Area of the rectangle are", sum);
+// console.log(calculateAreaofRectangle(5, 70));
+
+//4-
+// let str = "Hello World!";
+// function Numbers(str) {
+//   return str.split("").reverse().join();
+// }
+
+// console.log(Numbers(str));
+
+//5-  Check Number is even or odd
+// function chechNum(num) {
+//   if (num % 2 == 0) {
+//     return "Number is Even " + num;
+//   } else {
+//     return "Number is odd " + num;
+//   }
+// }
+// console.log(chechNum(6));
+
+//6
+// let arr = [3, 7, 5, 8, 4];
+// let sum = 0;
+// for (i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+// }
+// console.log(sum);
+
+//7 Creating an Object of q car
+// const car = {
+//   make: "Toyata",
+//   model: "Innova",
+//   year: 2015,
+// };
+// car.startEngine = function () {
+//   console.log("Engine Started");
+// };
+// car.startEngine();
+
+// 8 Change the text of a paragraph using javaScript
+// DOM Manupulation
+// const para = document.getElementById("para");
+// para.textContent = "Hello ";
+
+//9 - Create a button dynamicaaly and add in to the webpage
